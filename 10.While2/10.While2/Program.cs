40374d02a6fbae31a73430078da69c76b47e0480
Nikooo-rp <nicolas.rojasp@upb.edu.co>
2025-08-29 09:39:53 -0500

    Actividad ciclo while: Suma de numeros impares hasta el 99

--- 10.While2/10.While2/Program.cs
+++ 10.While2/10.While2/Program.cs
@@ -38,6 +38,20 @@
 
             Console.WriteLine($"La suma total es: {suma}");
 
+            /*Realizar algoritmo que permita obtener la suma de todos los números impares hasta el 99.*/
+
+            int numim = 0;
+            int i2 = 1;
+
+            while (i2 < 100)
+            {
+                numim = numim + i2;
+                Console.WriteLine($"Num: {numim}");
+                i2 = i2 + 2;
+            }
+
+            Console.WriteLine($"La suma de los numeros impares hasta el 99 es {numim}");
+
         }
 
     }
