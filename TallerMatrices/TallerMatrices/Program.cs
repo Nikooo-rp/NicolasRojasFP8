3766e92159e3b3180761ba56e2571cc965b464d9
Nikooo-rp <nicolas.rojasp@upb.edu.co>
2025-10-01 08:29:10 -0500

    Inicio del taller de matrices

---
+++ TallerMatrices/TallerMatrices/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TallerMatrices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] numeros = new int[10, 20];
+
+            for (int i = 0; i < numeros.GetLength(0); i++)
+            {
+                for (int j = 0; j < numeros.GetLength(1); j++)
+                {
+                    Console.WriteLine($"Ingresa el valor para la posición {i}, {j}");
+                    numeros[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            for (int i = 0; i < numeros.GetLength(0); i++)
+            {
+                for (int j = 0; j < numeros.GetLength(1); j++)
+                {
+                    Console.WriteLine($"El valor de la posición {i}, {j} es {numeros[i, j]}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
